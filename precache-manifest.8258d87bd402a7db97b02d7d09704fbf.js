2900164b3566753670e1bf3a4fc7430465c52e26
Tuyen <1612792@student.hcmus.edu.vn>
2019-10-09 15:20:31 +0700

    Updates

---
+++ precache-manifest.8258d87bd402a7db97b02d7d09704fbf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf5a87f371fc52aa2b7800bd5dbafc1d",
+    "url": "/GameCaroRedux/index.html"
+  },
+  {
+    "revision": "acf38d89a6bd7b73e48e",
+    "url": "/GameCaroRedux/static/css/main.b8243afa.chunk.css"
+  },
+  {
+    "revision": "a078a601b120dc198cf4",
+    "url": "/GameCaroRedux/static/js/2.9d468323.chunk.js"
+  },
+  {
+    "revision": "acf38d89a6bd7b73e48e",
+    "url": "/GameCaroRedux/static/js/main.685875ef.chunk.js"
+  },
+  {
+    "revision": "aae37da37b25f17de67b",
+    "url": "/GameCaroRedux/static/js/runtime~main.42fd72d9.js"
+  }
+]);
